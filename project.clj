42e277aa539628e46caa766bfab1cefe5013829e
KARASZI István <github@spam.raszi.hu>
2015-10-16 16:24:24 +0200

    fix project dependencies

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [tentacles "0.3.0"]
                  [grimradical/clj-semver "0.3.0-20130920.191002-3" :exclusions [org.clojure/clojure]]]
-  :profiles {:dev {:dependencies [[org.clojure/ () "0.2.11"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}})
