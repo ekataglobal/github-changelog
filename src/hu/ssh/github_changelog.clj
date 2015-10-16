6e5282c54365cc7f934e3d48beaa1580b409aec8
KARASZI István <github@spam.raszi.hu>
2015-10-16 20:28:53 +0200

    sort the fetched keys by the version

--- src/hu/ssh/github_changelog.clj
+++ src/hu/ssh/github_changelog.clj
@@ -6,13 +6,13 @@
     [tentacles.pulls :as pulls]
     [clj-semver.core :as semver]))
 
-(defn repo
+(defn- repo
   "Gets the repository from its name"
   ([name] (repo "pro" name))
   ([org repo] [org repo]))
 
-(defn parse-semver
-  "Checks for semantic versions with or without v predicate"
+(defn- parse-semver
+  "Parse semantic versions with or without 'v' predicate from the tags"
   [tag]
   (let [version (:name tag)
         parse #(try (semver/parse %)
@@ -21,10 +21,21 @@
       (parse (apply str (rest version)))
       (parse version))))
 
+(defn- map-semver
+  "Map semver to the tag"
+  [tags]
+  (map #(assoc % :version (parse-semver %)) tags))
+
+(defn- fetch-version-tags
+  "Fetch the version tags in the correct order"
+  [user repo]
+  (let [sort-fn #(semver/older? (:version %1) (:version %2))]
+    (sort sort-fn (filter :version (map-semver (repos/tags user repo))))))
+
 (defn changelog
   "Fetches the changelog"
   [user repo]
-  (let [tags (delay (map #(assoc % :version (parse-semver %)) (repos/tags user repo)))
+  (let [tags (delay (fetch-version-tags user repo))
         pulls (delay (pulls/pulls user repo {:state "closed"}))
         commits (delay (repos/commits user repo))]
     (println (first @tags))))
