830d82c26b512fd173c7929caf04acbd28f1bf80
KARASZI István <github@spam.raszi.hu>
2015-10-20 10:02:01 +0200

    small reorganizations

--- src/hu/ssh/github_changelog/core.clj
+++ src/hu/ssh/github_changelog/core.clj
@@ -2,8 +2,40 @@
   (:require
     [hu.ssh.github-changelog.util :as util]
     [hu.ssh.github-changelog.git :as git]
+    [hu.ssh.github-changelog.semver :as semver]
     [hu.ssh.github-changelog.github :as github]
     [environ.core :refer [env]]))
+
+(defn- assoc-semver [tag]
+  {:pre  [(map? tag) (:name tag)]
+   :post [(contains? % :version)]}
+  (assoc tag :version (semver/extract (:name tag))))
+
+(defn- assoc-ranges [tags]
+  {:pre [(seq? tags)]}
+  (let [previous-shas (concat (map :sha (rest tags)) [nil])]
+    (map #(assoc %1 :from %2) tags previous-shas)))
+
+(defn parse-tags [tags]
+  {:pre  [(seq? tags)]
+   :post [(every? map? %)]}
+  (->> (map assoc-semver tags)
+       (filter :version)
+       (sort-by :version semver/newer?)
+       assoc-ranges))
+
+(defn- assoc-commits [git tag]
+  {:pre  [(map? tag)]
+   :post [(:commits %)]}
+  (assoc tag :commits (git/commits git (:from tag) (:sha tag))))
+
+(defn- load-tags [user repo]
+  {:pre  [(string? user) (string? repo)]
+   :post [(seq? %)]}
+  (let [git (git/clone (util/git-url prefix user repo))
+        tags (git/version-tags git)]
+    (->> (parse-tags tags)
+         (map (partial assoc-commits git)))))
 
 (defn- find-pull
   [pulls sha]
@@ -17,24 +49,6 @@
                            (map (partial find-pull pulls))
                            (remove nil?))]
     (assoc tag :pulls related-pulls)))
-
-(defn- assoc-ranges [tags]
-  {:pre [(seq? tags)]}
-  (let [previous-shas (concat (map :sha (rest tags)) [nil])]
-    (map #(assoc %1 :from %2) tags previous-shas)))
-
-(defn- assoc-commits [git tag]
-  {:pre  [(map? tag)]
-   :post [(:commits %)]}
-  (assoc tag :commits (git/commits git (:from tag) (:sha tag))))
-
-(defn- load-tags [user repo]
-  {:pre  [(string? user) (string? repo)]
-   :post [(seq? %)]}
-  (let [git (git/clone (util/git-url prefix user repo))
-        tags (git/version-tags git)]
-    (->> (assoc-ranges tags)
-         (map (partial assoc-commits git)))))
 
 (def prefix "https://github.com")
 
