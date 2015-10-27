cfaf4fa279dcbbeedf78f3faf439b5c025426d18
KARASZI István <github@spam.raszi.hu>
2015-10-27 20:47:08 +0100

    add link method

--- src/hu/ssh/github_changelog/markdown.clj
+++ src/hu/ssh/github_changelog/markdown.clj
@@ -1,3 +1,7 @@
 (ns hu.ssh.github-changelog.markdown)
 
 (defn header [n body] (str (apply str (repeat n "#")) " " body))
+
+(defn link
+  ([url] (link url url))
+  ([text url] (format "[%s](%s)" text url)))
--- test/hu/ssh/github_changelog/markdown_test.clj
+++ test/hu/ssh/github_changelog/markdown_test.clj
@@ -7,3 +7,7 @@
   (are [expected level body] (= expected (markdown/header level body))
                              "# Header" 1 "Header"
                              "## SubHeader" 2 "SubHeader"))
+
+(deftest link
+  (is (= "[foo bar](http://example.com/)" (markdown/link "foo bar" "http://example.com/")))
+  (is (= "[http://example.com/](http://example.com/)" (markdown/link "http://example.com/"))))
