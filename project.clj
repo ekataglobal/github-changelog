660583f750ce9fa8814ea1a0a8c8bca8c3e23c10
KARASZI István <github@spam.raszi.hu>
2015-11-16 09:25:34 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
             :url "http://choosealicense.com/licenses/mit/"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [prismatic/schema "1.0.2"]
+                 [prismatic/schema "1.0.3"]
                  [environ "1.0.1"]
                  [org.clojure/tools.cli "0.3.3"]
                  [clj-jgit "0.8.8"]
-                 [tentacles "0.3.0"]
+                 [tentacles "0.4.0"]
                  [grimradical/clj-semver "0.3.0-20130920.191002-3" :exclusions [org.clojure/clojure]]
-                 [org.clojure/test.check "0.8.2"]]
+                 [org.clojure/test.check "0.9.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}
              :uberjar {:aot :all}})
