27af60456b5fa26a94a056ab2bbc5ece901791c8
KARASZI István <github@spam.raszi.hu>
2016-01-09 12:31:18 +0100

    Add strip-trailing function

--- src/hu/ssh/github_changelog/util.clj
+++ src/hu/ssh/github_changelog/util.clj
@@ -6,3 +6,8 @@
 (defn gen-sha [] (join (repeatedly 40 #(rand-nth "0123456789ABCDEF"))))
 
 (defn str-map [f & sqs] (join (apply map f sqs)))
+
+(defn strip-trailing [str end]
+  (if (.endsWith str end)
+    (.substring str 0 (- (count str) (count end)))
+    str))
