262b6f25e1c9cc0d729876a4bf12856b00799dc1
KARASZI István <github@spam.raszi.hu>
2016-03-04 16:05:32 +0100

    Extract hexadecimal generator

--- src/github_changelog/schema_generators.clj
+++ src/github_changelog/schema_generators.clj
@@ -4,7 +4,10 @@
             [schema.experimental.generators :as gen]
             [clojure.test.check.generators :as check-generators]))
 
+(def hexadecimal
+  (check-generators/elements "0123456789ABCDEF"))
+
 (def sha
-  ((gen/fmap join) (check-generators/vector (check-generators/elements "0123456789ABCDEF") 40)))
+  ((gen/fmap join) (check-generators/vector hexadecimal 40)))
 
 (def generators {schema/Sha sha})
