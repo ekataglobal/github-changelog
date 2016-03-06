662911ca0cfa38607547d98930c88c0cbcf35a58
KARASZI István <github@spam.raszi.hu>
2016-03-06 18:28:40 +0100

    Add tests for util/extract-params

--- test/github_changelog/util_test.clj
+++ test/github_changelog/util_test.clj
@@ -5,3 +5,7 @@
 (deftest strip-trailing
   (is (= "something" (util/strip-trailing "something///" "/")))
   (is (= "foobar" (util/strip-trailing "foobar" "!!"))))
+
+(deftest extract-params
+  (is (= {}) (util/extract-params ""))
+  (is (= {:a 1 :b 2}) (util/extract-params "a=1&b=2")))
