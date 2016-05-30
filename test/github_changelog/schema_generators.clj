bd286dc1dbaf0fe6b5074b2b5778e05abf89ce42
KARASZI István <github@spam.raszi.hu>
2016-05-30 10:50:29 +0200

    More sane generators

--- test/github_changelog/schema_generators.clj
+++ test/github_changelog/schema_generators.clj
@@ -2,11 +2,21 @@
   (:require [clojure.string :refer [join]]
             [clojure.test.check.generators :as gen]))
 
+(def string-std (gen/not-empty gen/string-alphanumeric))
+
 (def hexadecimal (gen/elements "0123456789ABCDEF"))
 
 (def sha (gen/fmap join (gen/vector hexadecimal 40)))
 
-(def issue (gen/tuple gen/string gen/string))
+(def issue (gen/tuple string-std string-std))
+
+(def revert-title (gen/fmap (partial format "Revert %s") string-std))
+
+(def revert-body (gen/fmap (partial apply format "Reverts %s/%s#%d")
+                           (gen/tuple string-std string-std gen/nat)))
+
+(def string-title (gen/fmap (partial apply format "%s(%s): %s")
+                            (gen/tuple string-std string-std string-std)))
 
 (defn complete
   ([gen] #(complete % gen))
@@ -19,8 +29,8 @@
      (def ~(symbol (str "complete-" fn-name)) (complete ~fn-name))))
 
 (defgen config
-  :user gen/string
-  :repo gen/string)
+  :user string-std
+  :repo string-std)
 
 (defgen semver
   :major gen/nat
@@ -28,19 +38,33 @@
   :patch gen/nat)
 
 (defgen pull
-  :title (gen/not-empty gen/string-alphanumeric)
+  :title string-std
   :number gen/nat
   :sha sha
-  :body gen/string
-  :html_url gen/string)
+  :body string-std
+  :html_url string-std)
+
+(defgen valid-pull
+  :title string-title
+  :number gen/nat
+  :sha sha
+  :body string-std
+  :html_url string-std)
+
+(defgen revert-pull
+  :title revert-title
+  :number gen/nat
+  :sha sha
+  :body revert-body
+  :html_url string-std)
 
 (defgen change
-  :type gen/string
-  :scope gen/string
-  :subject gen/string
+  :type gen/string-ascii
+  :scope gen/string-ascii
+  :subject gen/string-ascii
   :pull-request pull
   :issues (gen/vector issue))
 
 (defgen tag
-  :name (gen/not-empty gen/string-alphanumeric)
+  :name string-std
   :sha sha)
