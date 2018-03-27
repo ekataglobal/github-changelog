0e592360f2a34c0f0ef429d94f04d1d49d4af42d
Janos Meszaros <meszaros.jns@gmail.com>
2018-03-27 14:23:23 +0200

    Adding Dockerfile and release script

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM openjdk:latest
+
+ARG VERSION
+
+WORKDIR /usr/local/github-changelog
+
+RUN apt-get update
+RUN apt-get install --assume-yes ca-certificates curl
+
+COPY target/github-changelog-${VERSION}.jar ./github-changelog.jar
---
+++ docker_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export `tail -1 version.properties`
+
+boot uberjar
+
+docker build -t whitepages/github-changelog:${VERSION} -t whitepages/github-changelog:latest --build-arg VERSION=$VERSION .
+
+docker push whitepages/github-changelog:${VERSION}
+docker push whitepages/github-changelog:latest
