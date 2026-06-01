590c35eca6f25480a2d116de92bbec0ad35a1053
Imre Kószó <imrekoszo@users.noreply.github.com>
2026-06-01 19:26:57 +0200

    refactor: move test-only fs ops to test root

--- test/github_changelog/fs_test.clj
+++ test/github_changelog/fs_test.clj
@@ -1,65 +1,66 @@
 (ns github-changelog.fs-test
   (:require [clojure.string :as str]
             [clojure.test :refer [deftest is testing]]
-            [github-changelog.fs :as sut]))
+            [github-changelog.fs :as sut]
+            [github-changelog.test-fs :as test-fs]))
 
 (defn- file-tests [file]
   (is (string? file))
-  (is (sut/exists? file))
-  (is (sut/file? file)))
+  (is (test-fs/exists? file))
+  (is (test-fs/file? file)))
 
 (deftest tmp-file
   (testing "without arguments"
-    (let [file (sut/tmp-file)
-          name (sut/basename file)]
+    (let [file (test-fs/tmp-file)
+          name (test-fs/basename file)]
       (file-tests file)
       (is (str/ends-with? name ".tmp"))
-      (sut/rm file)))
+      (test-fs/rm file)))
   (testing "with prefix"
-    (let [file (sut/tmp-file nil "prefix")
-          name (sut/basename file)]
+    (let [file (test-fs/tmp-file nil "prefix")
+          name (test-fs/basename file)]
       (file-tests file)
       (is (str/starts-with? name "prefix"))
       (is (str/ends-with? name ".tmp"))
-      (sut/rm file)))
+      (test-fs/rm file)))
   (testing "with prefix and postfix"
-    (let [file (sut/tmp-file nil "prefix-" ".postfix")
-          name (sut/basename file)]
+    (let [file (test-fs/tmp-file nil "prefix-" ".postfix")
+          name (test-fs/basename file)]
       (file-tests file)
       (is (str/starts-with? name "prefix-"))
       (is (str/ends-with? name ".postfix"))
-      (sut/rm file))))
+      (test-fs/rm file))))
 
 (defn- dir-tests [dir]
   (is (string? dir))
-  (is (sut/exists? dir))
+  (is (test-fs/exists? dir))
   (is (sut/dir? dir)))
 
 (deftest tmp-dir
   (testing "without arguments"
-    (let [dir (sut/tmp-dir)]
+    (let [dir (test-fs/tmp-dir)]
       (try
         (dir-tests dir)
         (finally
-          (sut/rm dir)))))
+          (test-fs/rm dir)))))
   (testing "with prefix"
-    (let [dir  (sut/tmp-dir nil "github-changelog_")
-          name (sut/basename dir)]
+    (let [dir  (test-fs/tmp-dir nil "github-changelog_")
+          name (test-fs/basename dir)]
       (try
         (dir-tests dir)
         (is (str/starts-with? name "github-changelog_"))
         (finally
-          (sut/rm dir))))))
+          (test-fs/rm dir))))))
 
 (deftest dir?
   (is (sut/dir? (System/getProperty "java.io.tmpdir")))
   (is (not (sut/dir? (:file (meta #'dir?))))))
 
 (deftest rm-dir
-  (let [dir  (sut/tmp-dir)
-        file (sut/tmp-file dir)]
+  (let [dir  (test-fs/tmp-dir)
+        file (test-fs/tmp-file dir)]
     (dir-tests dir)
     (file-tests file)
-    (sut/rm-dir dir)
-    (is (not (sut/exists? file)))
-    (is (not (sut/exists? dir)))))
+    (test-fs/rm-dir dir)
+    (is (not (test-fs/exists? file)))
+    (is (not (test-fs/exists? dir)))))
--- test/github_changelog/git_helper.clj
+++ test/github_changelog/git_helper.clj
@@ -1,12 +1,12 @@
 (ns github-changelog.git-helper
   (:require [clojure.java.shell :as shell]
-            [github-changelog.fs :as fs])
+            [github-changelog.test-fs :as test-fs])
   (:import java.util.UUID))
 
 (defn init-repo []
-  (let [dir  (fs/tmp-dir nil "github-changelog-repo_")
-        file (fs/tmp-file dir)
-        name (fs/basename file)]
+  (let [dir  (test-fs/tmp-dir nil "github-changelog-repo_")
+        file (test-fs/tmp-file dir)
+        name (test-fs/basename file)]
     (shell/with-sh-dir dir
       (shell/sh "git" "init" ".")
       (shell/sh "git" "add" name)
@@ -14,8 +14,8 @@
     [dir name]))
 
 (defn add-file [repo]
-  (let [file (fs/tmp-file repo)
-        name (fs/basename file)]
+  (let [file (test-fs/tmp-file repo)
+        name (test-fs/basename file)]
     (shell/with-sh-dir repo
       (shell/sh "git" "add" name)
       (shell/sh "git" "commit" "-m" (format "%s addded" name)))
